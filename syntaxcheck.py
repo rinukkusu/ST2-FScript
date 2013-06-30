b73cf01cd6c271e0f35ae8835524957f7287f5d5
rinukkusu@home <rinukkusu@sub-r.de>
2013-07-01 01:07:02 +0200

    syntax check now in alpha state

--- syntaxcheck.py
+++ syntaxcheck.py
@@ -1,10 +1,29 @@
 import sublime, sublime_plugin
+import re
+
 
 class SyntaxCheck(sublime_plugin.EventListener):
-	def on_modified(self, view):
+	def on_pre_save(self, view):
 		if "Future" in view.settings().get('syntax'):
-			# print status in stdout (debug only)
-			print "is Future Syntax: true"
+			if view.settings().get('SyntaxCheckOnSave'):
+				# print status in stdout (debug only)
+				print "is Future Syntax: true"
 
-			# TODO: check last char (;)
+				# TODO: check last char (;)
+				lines = re.split("\n", view.substr(sublime.Region(0, view.size())))
+				print "lines:", len(lines)
 
+				errors = "";
+
+				cl = 0 # current line
+				while cl < len(lines):
+					line = lines[cl]
+					last_ch = lines[cl][len(lines[cl])-1:]
+
+					if last_ch not in ['{', ';']:
+						errors += "missing semicolon on line " + str(cl+1) + "\r\n"
+
+					cl += 1
+
+				if len(errors) > 0:
+					sublime.error_message(errors)
